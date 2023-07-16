1b4bdd2c92071507ecfd007c5f15b3fc5944d733
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-07-16 22:16:00 +0700

    added editable-button

---
+++ components/editable-button/src/EditableButton.tsx
@@ -0,0 +1,137 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// cssfn:
+import {
+    // style sheets:
+    dynamicStyleSheet,
+}                           from '@cssfn/cssfn-react'           // writes css in react hook
+
+// reusable-ui core:
+import {
+    // react helper hooks:
+    useMergeEvents,
+    useMergeClasses,
+    
+    
+    
+    // a possibility of UI having an invalid state:
+    InvalidableProps,
+    useInvalidable,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
+// reusable-ui components:
+import {
+    // simple-components:
+    ButtonIcon,
+    ButtonIconProps
+}                           from '@reusable-ui/components'  // a set of official Reusable-UI components
+
+
+
+// styles:
+export const useEditableButtonStyleSheet = dynamicStyleSheet(
+    () => import(/* webpackPrefetch: true */ './styles/styles.js')
+, { id: 'h5j3kpwec5' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
+
+
+
+
+export interface EditableButtonProps
+    extends
+        // bases:
+        ButtonIconProps,
+        
+        // states:
+        InvalidableProps
+{
+}
+const EditableButton = (props: EditableButtonProps) => {
+    // styles:
+    const styleSheet = useEditableButtonStyleSheet();
+    
+    
+    
+    const invalidableState = useInvalidable<HTMLButtonElement>(props);
+    
+    
+    
+    // rest props:
+    const {
+        // validations:
+        enableValidation  : _enableValidation,  // remove
+        isValid           : _isValid,           // remove
+        inheritValidation : _inheritValidation, // remove
+        onValidation      : _onValidation,      // remove
+    ...restButtonIconProps} = props;
+    
+    
+    
+    // classes:
+    const stateClasses = useMergeClasses(
+        // preserves the original `stateClasses`:
+        props.stateClasses,
+        
+        
+        
+        // states:
+        invalidableState.class,
+        styleSheet.main, // additional styleSheet
+    );
+    
+    
+    
+    // handlers:
+    const handleAnimationStart = useMergeEvents(
+        // preserves the original `onAnimationStart`:
+        props.onAnimationStart,
+        
+        
+        
+        // states:
+        invalidableState.handleAnimationStart,
+    );
+    const handleAnimationEnd   = useMergeEvents(
+        // preserves the original `onAnimationEnd`:
+        props.onAnimationEnd,
+        
+        
+        
+        // states:
+        invalidableState.handleAnimationEnd,
+    );
+    
+    
+    
+    // jsx:
+    return (
+        <ButtonIcon
+            // rest props:
+            {...restButtonIconProps}
+            
+            
+            
+            // appearances:
+            icon='dropdown'
+            iconPosition='end'
+            
+            
+            
+            // classes:
+            stateClasses={stateClasses}
+            
+            
+            
+            // handlers:
+            onAnimationStart = {handleAnimationStart}
+            onAnimationEnd   = {handleAnimationEnd  }
+        />
+    );
+};
+export {
+    EditableButton,
+    EditableButton as default,
+}
