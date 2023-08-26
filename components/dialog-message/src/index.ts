d6b9cb3b5b4faa4ad18611f2318bc915773af9d1
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-08-26 09:15:07 +0700

    deleted modal-status & dialog-message

--- components/dialog-message/src/index.ts
+++
@@ -1,9 +0,0 @@
-export *            from './types.js'
-export type {
-    // contexts:
-    DialogMessageApi,
-}                   from './DialogMessageContext.js'
-
-export *            from './DialogMessageConsumer.js'
-export *            from './DialogMessageProvider.js'
-export {default}    from './DialogMessageProvider.js'
--- components/modal-status/src/index.ts
+++
@@ -1,4 +0,0 @@
-// export *            from './hooks.js' // not exported
-
-export *            from './ModalStatus.js'
-export {default}    from './ModalStatus.js'
