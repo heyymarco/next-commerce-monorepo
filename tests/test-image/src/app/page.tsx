e8698e0b8a4197f6d6456ba50f4e6d36e7c3e1e8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-09-20 11:35:33 +0700

    added test-image

---
+++ tests/test-image/src/app/page.tsx
@@ -0,0 +1,31 @@
+'use client'
+
+// import Image from 'next/image'
+import { Image } from '@heymarco/image'
+import styles from './page.module.css'
+
+export default function Home() {
+    return (
+        <main className={styles.main}>
+            <div className={styles.test}>
+                <Image src='/lorem.jpg' alt='' />
+                <br />
+                <br />
+                <br />
+                <Image src='/lorem.jpg' alt='' width={225*1} height={160*1} />
+                <br />
+                <br />
+                <br />
+                <Image src='/lorem.jpg' alt='' width={225*1.5} height={160*1.5} />
+                <br />
+                <br />
+                <br />
+                <Image src='/lorem.jpg' alt='' width={225*1} height={160*1} style={{ width: '113px', height: '80px', background: 'pink' }} />
+                <br />
+                <br />
+                <br />
+                <Image src='/lorem.jpg' alt='' width={225*2} height={160*1} style={{ background: 'pink' }} />
+            </div>
+        </main>
+    )
+}
