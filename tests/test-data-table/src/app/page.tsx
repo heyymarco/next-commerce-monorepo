747b62edc8077d9924a95656789969f50a6c6dd8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-12 17:11:02 +0700

    updated test-data-table

--- tests/test-data-table/src/app/page.tsx
+++ tests/test-data-table/src/app/page.tsx
@@ -1,14 +1,17 @@
 'use client'
 
+import { useState } from 'react'
 // import Image from 'next/image'
 import { DataTable, DataTableBody, DataTableHeader } from '@heymarco/data-table'
 import styles from './page.module.css'
 
 export default function Home() {
+    const [enabled, setEnabled] = useState<boolean>(true);
+    const [active , setActive ] = useState<boolean>(false);
     return (
         <main className={styles.main}>
             <div className={styles.test}>
-                <DataTable theme='primary'>
+                <DataTable theme='success' enabled={enabled} active={active}>
                     <DataTableHeader>
                         <tr className="tr">
                             <th className="th">
@@ -54,6 +57,11 @@ export default function Home() {
                         </tr>
                     </DataTableBody>
                 </DataTable>
+                
+                <hr />
+                
+                <label htmlFor='chkEnb'><input id='chkEnb' type='checkbox' checked={enabled} onChange={({target: {checked}}) => setEnabled(checked)} /> enabled</label>
+                <label htmlFor='chkAct'><input id='chkAct' type='checkbox' checked={active } onChange={({target: {checked}}) =>  setActive(checked)} /> active</label>
                 
                 <hr />
                 
