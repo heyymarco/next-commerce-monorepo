269e13fa70408358b38b3667843e4a5c87783b69
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2023-12-19 11:40:21 +0700

    updated <DataTable>

--- components/data-table/src/styles/elements.ts
+++ components/data-table/src/styles/elements.ts
@@ -1,8 +1,8 @@
 // elements:
-export const theadElm = '.thead' // one degree specificity to overwrite <DataTableHeader> component
-export const tfootElm = '.tfoot' // one degree specificity to overwrite <DataTableFooter> component
-export const tbodyElm = '.tbody' // one degree specificity to overwrite <DataTableBody>   component
+export const theadElm = ':where(.thead)' // one degree specificity to overwrite <DataTableHeader> component
+export const tfootElm = ':where(.tfoot)' // one degree specificity to overwrite <DataTableFooter> component
+export const tbodyElm = ':where(.tbody)' // one degree specificity to overwrite <DataTableBody>   component
 
-export const trElm    = '.tr'    // one degree specificity to overwrite <tr>              component
-export const tdElm    = '.td'    // one degree specificity to overwrite <td>              component
-export const thElm    = '.th'    // one degree specificity to overwrite <th>              component
+export const trElm    = ':where(.tr)'    // one degree specificity to overwrite <tr>              component
+export const tdElm    = ':where(.td)'    // one degree specificity to overwrite <td>              component
+export const thElm    = ':where(.th)'    // one degree specificity to overwrite <th>              component
