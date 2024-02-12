00525f3ebc4c60e62f24ff38f6c1f90d20e69203
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-02-12 13:13:36 +0700

    added useControllable()

---
+++ tests/test-drag-n-drop/src/libs/events/controllable.ts
@@ -0,0 +1,76 @@
+// reusable-ui core:
+import {
+    // react helper hooks:
+    useEvent,
+}                           from '@reusable-ui/core'            // a set of reusable-ui packages which are responsible for building any component
+
+// internals:
+import {
+    // types:
+    ScheduleTriggerEventOptions,
+    
+    
+    
+    // hooks:
+    useScheduleTriggerEvent,
+}                           from './hooks'
+
+
+
+export interface ControllableProps<TValue extends any> {
+    // values:
+    value              : TValue
+    onValueChange      : ((newValue: TValue) => void)|undefined
+}
+export interface ControllableApi<TValue extends any> {
+    // values:
+    value              : TValue
+    triggerValueChange : (newValue: TValue, options?: ScheduleTriggerEventOptions) => void
+}
+export const useControllable = <TValue extends any>(props: ControllableProps<TValue>): ControllableApi<TValue> => {
+    // props:
+    const {
+        // values:
+        value         : controllableValue,
+        onValueChange : onControllableValueChange,
+    } = props;
+    
+    
+    
+    // states:
+    const value = controllableValue;
+    
+    
+    
+    // handlers:
+    const handleValueChange    = onControllableValueChange;
+    
+    
+    
+    // stable callbacks:
+    const scheduleTriggerEvent = useScheduleTriggerEvent();
+    const triggerValueChange   = useEvent((newValue: TValue, options?: ScheduleTriggerEventOptions): void => {
+        // conditions:
+        if (!handleValueChange) return; // no callback handler => nothing to trigger
+        
+        
+        
+        scheduleTriggerEvent(() => {
+            // fire `onControllableValueChange` react event:
+            handleValueChange(newValue);
+        }, options);
+    });
+    
+    
+    
+    // api:
+    return {
+        // values:
+        value,
+        
+        
+        
+        // stable callbacks:
+        triggerValueChange,
+    };
+};
--- tests/test-drag-n-drop/src/libs/events/index.ts
+++ tests/test-drag-n-drop/src/libs/events/index.ts
@@ -1,3 +1,4 @@
 export *    from './hooks'
 export *    from './synthetic-events'
 export *    from './controllable-uncontrollable'
+export *    from './controllable'
