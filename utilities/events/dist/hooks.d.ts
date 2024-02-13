35a56a520c6c859693abf59d207abeebdf617603
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-02-13 23:25:58 +0700

    Delete utilities/events/dist directory

--- utilities/events/dist/hooks.d.ts
+++
@@ -1,7 +0,0 @@
-export type TriggerAt = 'immediately' | 'microtask' | 'macrotask';
-export interface ScheduleTriggerEventOptions {
-    triggerAt?: TriggerAt;
-}
-export type ScheduledTriggerEventCallback = () => void;
-export type ScheduleTriggerEventFunction = (scheduledTriggerEventCallback: ScheduledTriggerEventCallback | null | undefined, options?: ScheduleTriggerEventOptions) => void;
-export declare const useScheduleTriggerEvent: () => ScheduleTriggerEventFunction;
--- utilities/events/dist/index.d.ts
+++
@@ -1,5 +0,0 @@
-export * from './hooks.js';
-export * from './synthetic-events.js';
-export * from './controllable-uncontrollable.js';
-export * from './controllable.js';
-export * from './uncontrollable.js';
