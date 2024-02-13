c05b1b4fa512d9cfe6d4a349f83806d4abdd4448
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-02-13 13:35:41 +0700

    removed unused code

--- tests/test-drag-n-drop/src/components/OrderableList/styles/loader.ts
+++ tests/test-drag-n-drop/src/components/OrderableList/styles/loader.ts
@@ -7,7 +7,6 @@ import {
 
 
 // styles:
-import './styles'; // TODO: remove this on production
 export const useOrderableListStyleSheet = dynamicStyleSheets(
     () => import(/* webpackPrefetch: true */ './styles')
 , { id: 'r04l1lsujh' }); // a unique salt for SSR support, ensures the server-side & client-side have the same generated class names
