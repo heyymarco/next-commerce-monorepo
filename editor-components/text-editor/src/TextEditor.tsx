7bca158dc8e0340584ca16f520c5b2ebafd99531
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-21 04:28:50 +0700

    added TextEditor

---
+++ editor-components/text-editor/src/TextEditor.tsx
@@ -0,0 +1,99 @@
+// react:
+import {
+    // react:
+    default as React,
+}                           from 'react'
+
+// reusable-ui core:
+import {
+    // react helper hooks:
+    useMergeEvents,
+}                           from '@reusable-ui/core'                    // a set of reusable-ui packages which are responsible for building any component
+
+// internals:
+import {
+    // react components:
+    type EditorProps,
+    Editor,
+}                           from '@heymarco/editor'
+
+
+
+// react components:
+export interface TextEditorProps<out TElement extends Element = HTMLSpanElement, in TChangeEvent extends React.SyntheticEvent<unknown, Event> = React.ChangeEvent<HTMLInputElement>>
+    extends
+        // bases:
+        EditorProps<TElement, TChangeEvent, string>
+{
+}
+const TextEditor = <TElement extends Element = HTMLSpanElement, TChangeEvent extends React.SyntheticEvent<unknown, Event> = React.ChangeEvent<HTMLInputElement>>(props: TextEditorProps<TElement, TChangeEvent>): JSX.Element|null => {
+    // rest props:
+    const {
+        // values:
+        onChange,
+        onChangeAsText,
+        
+        
+        
+        // other props:
+        ...restTextEditorProps
+    } = props;
+    
+    
+    
+    // handlers:
+    const handleChangeAsText = useMergeEvents(
+        // preserves the original `onChange` from `props`:
+        onChange,
+        
+        
+        
+        // preserves the original `onChangeAsText` from `props`:
+        onChangeAsText,
+    );
+    
+    
+    
+    // default props:
+    const {
+        // formats:
+        type = 'text',
+        
+        
+        
+        // other props:
+        ...restEditorProps
+    } = restTextEditorProps;
+    
+    
+    
+    // jsx:
+    return (
+        <Editor<TElement, TChangeEvent, string>
+            // other props:
+            {...restEditorProps}
+            
+            
+            
+            // values:
+            onChangeAsText={handleChangeAsText}
+            
+            
+            
+            // formats:
+            type={type}
+        />
+    );
+};
+export {
+    TextEditor,            // named export for readibility
+    TextEditor as default, // default export to support React.lazy
+}
+
+
+
+export interface TextEditorComponentProps<out TElement extends Element = HTMLSpanElement, in TChangeEvent extends React.SyntheticEvent<unknown, Event> = React.ChangeEvent<HTMLInputElement>>
+{
+    // components:
+    textEditorComponent ?: React.ReactElement<TextEditorProps<TElement, TChangeEvent>>
+}
---
+++ editor-components/text-editor/src/index.ts
@@ -0,0 +1,2 @@
+export *            from './TextEditor.js'
+export {default}    from './TextEditor.js'
