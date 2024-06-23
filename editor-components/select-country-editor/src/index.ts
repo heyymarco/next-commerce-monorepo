3e03e3568a9d4d3aaefc989862dd4b5aaabd64c7
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-24 05:24:31 +0700

    removed unused code

--- editor-components/select-country-editor/src/index.ts
+++ editor-components/select-country-editor/src/index.ts
@@ -2,5 +2,3 @@ export *            from './utilities.js'
 
 export *            from './SelectCountryEditor.js'
 export {default}    from './SelectCountryEditor.js'
-
-// export *            from './CountryDisplay.js'
