26ca5e1d4d3339634f65f2732b820ede3c86cbe8
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-06-28 03:49:50 +0700

    updated test-address-editor

--- tests/test-address-editor/src/app/page.tsx
+++ tests/test-address-editor/src/app/page.tsx
@@ -18,6 +18,7 @@ export default function Home() {
         test
       </p>
       <AddressEditor
+        addressType='shipping'
         countryEditorComponent={
           <SelectCountryEditor enableValidation />
         }
