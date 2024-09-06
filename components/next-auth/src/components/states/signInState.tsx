0d16c6c2267d3bb9a5bcad2bb7c1f698e7834840
HeyMarco <31385284+heyymarco@users.noreply.github.com>
2024-09-06 09:52:21 +0700

    removed useless auto redirect feature

--- components/next-auth/src/components/states/signInState.tsx
+++ components/next-auth/src/components/states/signInState.tsx
@@ -38,7 +38,6 @@ import type {
 import {
     // apis:
     signIn,
-    useSession,
 }                           from 'next-auth/react'
 
 // reusable-ui core:
@@ -551,11 +550,6 @@ const SignInStateProvider = (props: React.PropsWithChildren<SignInStateProps>) =
     const [isBusy           , setIsBusyInternal  ] = useState<BusyState>(false);
     
     const isMounted                                = useMountedFlag();
-    
-    
-    
-    // sessions:
-    const { status: sessionStatus } = useSession();
     
     
     
@@ -1202,30 +1196,6 @@ const SignInStateProvider = (props: React.PropsWithChildren<SignInStateProps>) =
         setPassword('');
         setPassword2('');
     }, [section]);
-    
-    const prevSessionStatusRef = useRef<typeof sessionStatus>(sessionStatus);
-    useEffect(() => {
-        // conditions:
-        if (prevSessionStatusRef.current === sessionStatus) return; // no change => ignore
-        const prevSessionStatus = prevSessionStatusRef.current;     // save the prev value before syncing
-        prevSessionStatusRef.current = sessionStatus;               // sync
-        
-        
-        
-        /*
-            refresh signIn page from already have signed in:
-            'loading'         => 'authenticated'
-            
-            after clicking doSignIn button and successfully signed in:
-            'unauthenticated' => 'authenticated' (already redirected by doSignIn button)
-        */
-        // transition from 'loading' => 'authenticated' => redirect to origin page:
-        if ((prevSessionStatus === 'loading') && (sessionStatus === 'authenticated')) {
-            if (callbackUrl) {
-                router.replace(callbackUrl);
-            } // if
-        } // if
-    }, [sessionStatus]);
     
     
     
